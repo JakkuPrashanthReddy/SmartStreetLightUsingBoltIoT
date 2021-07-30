9ebeaff6f9d376b121c4f433b16aa6dae6325131
Jakku Prashanth Reddy <67828370+PrashanthReddy03@users.noreply.github.com>
2021-07-30 16:16:25 +0530

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,17 @@
+# Bolt Configuration
+
+bolt_api_key = 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'         # You Bolt API key
+device_id = 'BOLTxxxxxxx'                                    # Your Bolt Device Name
+PIN = '0'
+
+# telegram Configuration
+
+chat_id = '@xxxxxxxxxxxxx'                                    # Your Telegram Chat ID
+bot_id = 'botxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'         # Your Bot Access Token
+
+''' NOTE : add prefix "bot" to your Bot access token
+    EXAMPLE : botXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX    '''
+
+# Threshold
+minimum = 20            #Threshold Values may Vary so Check with that and Configure the minimum and maximum
+maximum = 120           #these threshold values are under Room Light
