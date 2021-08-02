87ffe1254c4042928edff0052ed43b0befc893c4
Jakku Prashanth Reddy <67828370+PrashanthReddy03@users.noreply.github.com>
2021-08-02 13:23:52 +0530

    Added Anomaly Configuration

--- configuration.py
+++ configuration.py
@@ -15,3 +15,7 @@ bot_id = 'botxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'         # Your Bot Access 
 # Threshold
 minimum = 20            #Threshold Values may Vary so Check with that and Configure the minimum and maximum
 maximum = 120           #these threshold values are under Room Light
+
+#Anomaly
+Multiplication_factor = 15
+frame_size = 100
